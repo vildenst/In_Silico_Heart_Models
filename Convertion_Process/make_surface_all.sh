eadcf76e24fc82ff0d457301e5df4c46b09a70cb
Vilde Strøm <vilde@Vildes-MacBook-Pro.local>
2017-07-10 08:54:50 +0200

    updating convertion process

---
+++ Convertion_Process/make_surface_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p ./Data/vtkFiles
+mkdir -p ./Data/plyFiles
+mkdir -p ./Data/txtFiles
+
+
+for f in *.txt
+	do
+		python make_surface.py "$f"
+
+		'./ConvertFile/build/ConvertFile.app/Contents/MacOS/ConvertFile' "${f/.txt}".ply "${f/.txt}".vtk
+		mv "${f/.txt}".vtk ./Data/vtkFiles
+		mv "${f/.txt}".ply ./Data/plyFiles
+	    	mv $f ./Data/txtFiles
+done
+
+
