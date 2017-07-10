a0477785905dffd856011a8fc5e077948d41b732
Vilde Strøm <vilde@Vildes-MacBook-Pro.local>
2017-07-10 15:13:12 +0200

    updating

--- software.sh
+++ software.sh
@@ -1,8 +1,8 @@
 #!bin/bash -l
-#SBATCH --job-name=gmsh
+#SBATCH --job-name=SW
 #SBATCH --account=nn9249k
 #SBATCH --time=10:00:00
-#SBATCH --nodes=1 --ntasks-per-node=8
+#SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=4G
 
 #SBATCH --output=out.txt
