c9b21ab205d69edbce7f00f60353765d35ec84c1
Vilde Strøm <vilde@Vildes-MacBook-Pro.local>
2017-07-10 15:17:30 +0200

    updating

--- software.sh
+++ software.sh
@@ -1,12 +1,12 @@
 #!bin/bash -l
-#SBATCH --job-name=SW
+#SBATCH --job-name=SW2
 #SBATCH --account=nn9249k
 #SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4G
 
-#SBATCH --output=out.txt
-#SBATCH --error=err.txt
+#SBATCH --output=out2.txt
+#SBATCH --error=err2.txt
 
 source /cluster/bin/jobsetup
 
