e59cd7b4515a44252d502bb70a6a1aa2e3271d05
Vilde Strøm <vilde@Vildes-MacBook-Pro.local>
2017-07-11 11:38:00 +0200

    testing

--- mat2fem.py
+++ mat2fem.py
@@ -11,48 +11,48 @@ print('####################################')
 source ='{}/seg/'.format(root)
 files=os.listdir(source)
 N=len(files)-1	#number of .mat files to be processed
-nr_errors=0
-err_list=[]
-dest='{}/Matlab_Process/Data/Seg/'.format(root)
-for f in files:
-	shutil.copy(source+f,dest+f)	#all .mat files moved to dest
+print(N)
+# nr_errors=0
+# err_list=[]
+# dest='{}/Matlab_Process/Data/Seg/'.format(root)
+# for f in files:
+# 	shutil.copy(source+f,dest+f)	#all .mat files moved to dest
 
-os.chdir('Matlab_Process')
+# os.chdir('Matlab_Process')
 
-def run_matlab(nr_errors,err_list):
-	os.system('sh run_matlab.sh')	#running the matlab script
-	scar_folder ='{}/Matlab_Process/Data/ScarImages/MetaImages/'.format(root)
-	scar_files=os.listdir(scar_folder)
-	N2=len(scar_files)-1
-	if (N-nr_errors)*2 > N2:	#not all .mat files are processed
-		remove_error(N2,nr_errors,err_list)
+# def run_matlab(nr_errors,err_list):
+# 	os.system('sh run_matlab.sh')	#running the matlab script
+# 	scar_folder ='{}/Matlab_Process/Data/ScarImages/MetaImages/'.format(root)
+# 	scar_files=os.listdir(scar_folder)
+# 	N2=len(scar_files)-1
+# 	print(N2)
+# 	if (N-nr_errors)*2 > N2:	#not all .mat files are processed
+# 		remove_error(N2,nr_errors,err_list)
 
-def remove_error(N2,nr_errors,err_list):
-		error=(N2+nr_errors*2)/2+1
-		err_path='{}Patient_{}.mat'.format(dest,error)
-		os.remove(err_path)		
-		nr_errors+=1
-		err_list.append(error)
-		run_matlab(nr_errors,err_list)
+# def remove_error(N2,nr_errors,err_list):
+# 		error=(N2+nr_errors*2)/2+1
+# 		err_path='{}Patient_{}.mat'.format(dest,error)
+# 		os.remove(err_path)		
+# 		nr_errors+=1
+# 		err_list.append(error)
+# 		run_matlab(nr_errors,err_list)
 
-def remove_error_files(fname):
-	rm_path='{}/Data/Texts/'.format(os.getcwd())
-	for i in ['LVEndo','LVEpi','RVEndo','RVEpi']:
-		if os.path.isfile('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i)):
-			os.remove('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i))
+# def remove_error_files(fname):
+# 	rm_path='{}/Data/Texts/'.format(os.getcwd())
+# 	for i in ['LVEndo','LVEpi','RVEndo','RVEpi']:
+# 		if os.path.isfile('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i)):
+# 			os.remove('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i))
 
-run_matlab(nr_errors,err_list)
-if err_list:	#errors occured
-	for nr in err_list:
-		fname='Patient_{}'.format(nr)
-		remove_error_files(fname)
-		print('{} was removed due to errors. Will continue without it.'.format(fname))
+# run_matlab(nr_errors,err_list)
+# if err_list:	#errors occured
+# 	for nr in err_list:
+# 		fname='Patient_{}'.format(nr)
+# 		remove_error_files(fname)
+# 		print('{} was removed due to errors. Will continue without it.'.format(fname))
 
-# print('####################################')
+# # print('####################################')
 # print('######PART 2: MAKING SURFACES#######')
 # print('####################################')
-
-# print('All .vtk files are stored in Surfaces/vtkFiles')
 
 # import datetime
 # now=datetime.datetime.now()
@@ -60,11 +60,12 @@ if err_list:	#errors occured
 # os.chdir('..')
 # os.system('sh Process.sh {}'.format(time))	#running bash script
 
+# print('All .vtk files are stored in Surfaces/vtkFiles')
 # print('####################################')
 # print('###PART 3: GENERATING FEM MODELS####')
 # print('####################################')
 
-# print('All .msh and .out.txt files are stored in Surfaces/mshFiles')
+
 # #Generating .msh files from .vtk files
 # def mergevtk(i,msh_src):	
 # 	lv_endo='{}/Patient_{}-LVEndo-Frame_1.vtk'.format(src,i)
@@ -94,3 +95,4 @@ if err_list:	#errors occured
 # 	os.mkdir('{}/Patient_{}'.format(fem_src,i))
 # 	for j in ['tris', 'elem', 'pts']:
 # 		os.rename('Patient_{}.{}'.format(i,j), '{}/Patient_{}/Patient_{}.{}'.format(fem_src,i,i,j))
+# print('All .msh and .out.txt files are stored in Surfaces/mshFiles')
