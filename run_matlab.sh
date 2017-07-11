9f69e9128778a3eaeb12b0333bef00f12ac7d7db
Vilde Strøm <vilde@Vildes-MacBook-Pro.local>
2017-07-11 09:08:36 +0200

    updating path

--- run_matlab.sh
+++ run_matlab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #/Applications/MATLAB_R2016b.app/bin/matlab -nodisplay <alignAll.m &> logfile.output
-/cluster/software/matlab -nodisplay <alignAll.m &> logfile.output
+/cluster/software/matlab/matlab -nodisplay <alignAll.m &> logfile.output
