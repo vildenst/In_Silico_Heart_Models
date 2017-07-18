6f54f6724a336fdfbd8ebfb5196641956ce181bf
Vilde <vilde@Vildes-MBP>
2017-07-18 18:41:09 +0200

    adding pre-version

--- mat2fem.py
+++ mat2fem.py
@@ -7,128 +7,126 @@ import os
 import shutil 
 root=os.getcwd()	#path to In_Silico_Heart_Models
 
-time='18.07-17.44'
-N=1
-# """
-# PART 1: MATLAB SLICE ALIGNMENT
-# """
-# #running the matlab script alignAll.m
-# def run_matlab(nr_errors,err_list):
-# 	os.system('sh run_matlab.sh')	
-# 	scar_folder ='{}/Matlab_Process/Data/ScarImages/MetaImages/'.format(root)
-# 	scar_files=os.listdir(scar_folder)
-# 	N2=len(scar_files) #files produces from matlab. 
-# 	if (N-nr_errors)*2 > N2:	#not all .mat files are processed
-# 		remove_error(N2,nr_errors,err_list)
+"""
+PART 1: MATLAB SLICE ALIGNMENT
+"""
+#running the matlab script alignAll.m
+def run_matlab(nr_errors,err_list):
+	os.system('sh run_matlab.sh')	
+	scar_folder ='{}/Matlab_Process/Data/ScarImages/MetaImages/'.format(root)
+	scar_files=os.listdir(scar_folder)
+	N2=len(scar_files) #files produces from matlab. 
+	if (N-nr_errors)*2 > N2:	#not all .mat files are processed
+		remove_error(N2,nr_errors,err_list)
 
-# #if an error occured in matlab, this function removes the error file(s) and restarts
-# def remove_error(N2,nr_errors,err_list): 
-# 		error=(N2+nr_errors*2)/2+1
-# 		err_path='{}Patient_{}.mat'.format(dest,error)
-# 		os.remove(err_path)		
-# 		nr_errors+=1
-# 		err_list.append(error)
-# 		run_matlab(nr_errors,err_list)
+#if an error occured in matlab, this function removes the error file(s) and restarts
+def remove_error(N2,nr_errors,err_list): 
+		error=(N2+nr_errors*2)/2+1
+		err_path='{}Patient_{}.mat'.format(dest,error)
+		os.remove(err_path)		
+		nr_errors+=1
+		err_list.append(error)
+		run_matlab(nr_errors,err_list)
 
-# #if the error file(s) produced any text files, they're deleted here.
-# def remove_error_files(fname):
-# 	rm_path='{}/Data/Texts/'.format(os.getcwd())
-# 	for i in ['LVEndo','LVEpi','RVEndo','RVEpi']:
-# 		if os.path.isfile('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i)):
-# 			os.remove('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i))
+#if the error file(s) produced any text files, they're deleted here.
+def remove_error_files(fname):
+	rm_path='{}/Data/Texts/'.format(os.getcwd())
+	for i in ['LVEndo','LVEpi','RVEndo','RVEpi']:
+		if os.path.isfile('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i)):
+			os.remove('{}{}-{}-Frame_1.txt'.format(rm_path,fname,i))
 
-# source ='{}/seg/'.format(root) #.mat files source path
-# files=os.listdir(source)
-# N=len(files)	#number of .mat files to be processed
-# nr_errors=0
-# err_list=[]
-# dest='{}/Matlab_Process/Data/Seg/'.format(root) #.mat files dest path
-# for f in files:
-# 	shutil.copy(source+f,dest+f)	#move all .mat files to dest
+source ='{}/seg/'.format(root) #.mat files source path
+files=os.listdir(source)
+N=len(files)	#number of .mat files to be processed
+nr_errors=0
+err_list=[]
+dest='{}/Matlab_Process/Data/Seg/'.format(root) #.mat files dest path
+for f in files:
+	shutil.copy(source+f,dest+f)	#move all .mat files to dest
 
-# os.chdir('Matlab_Process')
+os.chdir('Matlab_Process')
 
-# run_matlab(nr_errors,err_list)
-# if err_list:	#errors occured
-# 	for nr in err_list:
-# 		fname='Patient_{}'.format(nr)
-# 		remove_error_files(fname)
-# 		print('{} was removed due to errors. Will continue without it.'.format(fname))
+run_matlab(nr_errors,err_list)
+if err_list:	#errors occured
+	for nr in err_list:
+		fname='Patient_{}'.format(nr)
+		remove_error_files(fname)
+		print('{} was removed due to errors. Will continue without it.'.format(fname))
 
-# print('#############################')
-# print('#PART 1 DONE: SLICES ALIGNED#')
-# print('#############################')
+print('#############################')
+print('#PART 1 DONE: SLICES ALIGNED#')
+print('#############################')
 
 
-# """
-# PART 2: SURFACE GENERATION
-# """
-# #moves all files in a directory
-# def move(src,dst):
-# 	src_files=os.listdir(src)
-# 	for f in src_files:
-# 		shutil.move(src+'/'+f,dst+'/'+f)
+"""
+PART 2: SURFACE GENERATION
+"""
+#moves all files in a directory
+def move(src,dst):
+	src_files=os.listdir(src)
+	for f in src_files:
+		shutil.move(src+'/'+f,dst+'/'+f)
 
-# #runs a bash program with a given path
-# def run(path,script):
-# 	os.chdir(path)
-# 	os.system('sh {}'.format(script))
-
-
-# #removes all files in a list of folders
-# def remove(folder_list,matpath):
-# 	for i in folder_list:
-# 		path=matpath+'/Data/'+i
-# 		files=os.listdir(path)
-# 		for f in files:
-# 			os.remove(path+'/'+f)
-
-# #creating new output folder for the surface data
-# import datetime
-# now=datetime.datetime.now()
-# time = now.strftime("%d.%m-%H.%M")
-# Date='Data-'+time	#name of output folder
-# print('Creating folder {} for data storage'.format(Date))
-# newdir=root+'/Surfaces/'+Date
-# os.mkdir(newdir)
-
-# #folder paths
-# matpath=root+'/Matlab_Process'
-# Conv_src=matpath+'/Data/Texts'
-# Scar_src=matpath+'/Data/ScarImages/MetaImages'
-# Conv_path=root+'/Convertion_Process'
-# Scar_path=root+'/Scar_Process/Data'
-
-# #moving and deleting files before starting surface generation
-# move(Conv_src,Conv_path)
-# move(Scar_src,Scar_path)
-# remove(['Aligned','Seg'],matpath)
-
-# #generating and moving heart surfaces
-# print('Making surfaces')
-# run(Conv_path,'make_surface_all.sh')
-# print('Moving files to Surfaces')
-# for i in ['plyFiles','vtkFiles','txtFiles']:
-# 	os.chdir(Conv_path+'/Data/'+i)
-# 	os.mkdir(newdir+'/'+i)
-# 	move(os.getcwd(),newdir+'/'+i)
-
-# #generating and moving scar surfaces
-# print('Making scar surfaces')
-# run(root+'/Scar_Process','run.sh')
-# print('Moving scar files to Surfaces')
-# src=os.listdir(os.getcwd()+'/Data')
-# for f in src:
-# 	if f.endswith('.vtk'):
-# 		shutil.move(os.getcwd()+'/Data/'+f,newdir+'/vtkFiles/')
-# 	else:
-# 		os.remove(os.getcwd()+'/Data/'+f)
+#runs a bash program with a given path
+def run(path,script):
+	os.chdir(path)
+	os.system('sh {}'.format(script))
 
 
-# print('All .vtk files are stored in Surfaces/{}/vtkFiles'.format(Date))
-# print('##################################')
-# print('###PART 2 DONE: MAKING SURFACES###')
-# print('##################################')
+#removes all files in a list of folders
+def remove(folder_list,matpath):
+	for i in folder_list:
+		path=matpath+'/Data/'+i
+		files=os.listdir(path)
+		for f in files:
+			os.remove(path+'/'+f)
+
+#creating new output folder for the surface data
+import datetime
+now=datetime.datetime.now()
+time = now.strftime("%d.%m-%H.%M")
+Date='Data-'+time	#name of output folder
+print('Creating folder {} for data storage'.format(Date))
+newdir=root+'/Surfaces/'+Date
+os.mkdir(newdir)
+
+#folder paths
+matpath=root+'/Matlab_Process'
+Conv_src=matpath+'/Data/Texts'
+Scar_src=matpath+'/Data/ScarImages/MetaImages'
+Conv_path=root+'/Convertion_Process'
+Scar_path=root+'/Scar_Process/Data'
+
+#moving and deleting files before starting surface generation
+move(Conv_src,Conv_path)
+move(Scar_src,Scar_path)
+remove(['Aligned','Seg'],matpath)
+
+#generating and moving heart surfaces
+print('Making surfaces')
+run(Conv_path,'make_surface_all.sh')
+print('Moving files to Surfaces')
+for i in ['plyFiles','vtkFiles','txtFiles']:
+	os.chdir(Conv_path+'/Data/'+i)
+	os.mkdir(newdir+'/'+i)
+	move(os.getcwd(),newdir+'/'+i)
+
+#generating and moving scar surfaces
+print('Making scar surfaces')
+run(root+'/Scar_Process','run.sh')
+print('Moving scar files to Surfaces')
+src=os.listdir(os.getcwd()+'/Data')
+for f in src:
+	if f.endswith('.vtk'):
+		shutil.move(os.getcwd()+'/Data/'+f,newdir+'/vtkFiles/')
+	else:
+		os.remove(os.getcwd()+'/Data/'+f)
+
+
+print('All .vtk files are stored in Surfaces/{}/vtkFiles'.format(Date))
+print('##################################')
+print('###PART 2 DONE: MAKING SURFACES###')
+print('##################################')
 
 
 """
@@ -247,16 +245,16 @@ def write_files(patient_path,i):
 vtk_src='{}/Surfaces/Data-{}/vtkFiles'.format(root,time) #source .vtk files
 msh_src='Surfaces/Data-{}/mshFiles'.format(time) 
 fem_src='FEM/Data-{}'.format(time)
-#os.mkdir(root+'/'+msh_src)	#storing msh & msh output files here
-#os.mkdir(root+'/'+fem_src)	#storing pts, elem & tris files here
+os.mkdir(root+'/'+msh_src)	#storing msh & msh output files here
+os.mkdir(root+'/'+fem_src)	#storing pts, elem & tris files here
 
 for i in range(1,N+1):
-	#if os.path.isfile('{}/Patient_{}_scar.vtk'.format(vtk_src,i)):	#patient exists
-	#	mergevtk(i,'{}/{}'.format(root,msh_src),vtk_src)	#generation of .msh files
-	#	print('Generated .msh file for Patient {}.'.format(i))
+	if os.path.isfile('{}/Patient_{}_scar.vtk'.format(vtk_src,i)):	#patient exists
+		mergevtk(i,'{}/{}'.format(root,msh_src),vtk_src)	#generation of .msh files
+		print('Generated .msh file for Patient {}.'.format(i))
 
-	#generating pts, tris & elem files from msh files
-	#write_fem('{}/{}/Patient_{}.msh'.format(root,msh_src,i),'Patient_{}'.format(i))
+	generating pts, tris & elem files from msh files
+	write_fem('{}/{}/Patient_{}.msh'.format(root,msh_src,i),'Patient_{}'.format(i))
 	print('Generated .tris, .elem & .pts file for Patient {}.'.format(i))
 
 	#moving FEM files to correct folder
